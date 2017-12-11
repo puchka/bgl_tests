b8c31f12b0655a6f8c4b33c61be206f0c83c60c1
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2017-12-11 17:10:32 +0400

    Add non-named parameters

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <typeinfo>
 #include <utility>
-#include "boost/graph/adjacency_list.hpp"
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/breadth_first_search.hpp>
 
 using namespace boost;
 
 int main()
 {
-  adjacency_list<vecS, vecS, undirectedS> g;
+  typedef adjacency_list<vecS, vecS, undirectedS> Graph;
+  Graph g;
 
   enum { topLeft, topRight, bottomRight, bottomLeft };
   
@@ -19,6 +21,14 @@ int main()
   auto p = edges(g);
   for (auto it = p.first; it != p.second; ++it)
     std::cout << *it << std::endl;
+
+  queue<Graph::vertex_descriptor> q;
+  
+  bfs_visitor<null_visitor> vis;
+
+  std::array<default_color_type, 4> colormap;
+
+  breadth_first_search(g, topLeft, q, vis, colormap.data());
   
   return 0;
 }
